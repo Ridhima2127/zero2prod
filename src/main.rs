fb8752632872de9c1e8a0c6ebe48957074f27c23
Ridhima Khyalappa <ridhima_khyalappa@heartcore.co.jp>
2023-03-02 14:43:55 +0900

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_client(stream);
+            }
+            Err(e) => {
+                eprintln!("Failed to accept client: {}", e);
+            }
+        }
+    }
+}
+
+fn handle_client(stream: TcpStream) {
+    // Handle client connection
 }
